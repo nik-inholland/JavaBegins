5db441fd22e38396917312333c498fb54a30071a
nik-inholland <745045@student.inholland.nl>
2026-09-09 12:57:15 +0200

    updates

--- src/main/java/nl/inholland/nl/javabegins/LoginController.java
+++ src/main/java/nl/inholland/nl/javabegins/LoginController.java
@@ -1,4 +1,31 @@
 package nl.inholland.nl.javabegins;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    protected void onLoginButtonClick() {
+
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("admin") && password.equals("1234")) {
+            messageLabel.setText("Welcome, " + username);
+        } else {
+            messageLabel.setText("Invalid username/password");
+        }
+    }
 }
